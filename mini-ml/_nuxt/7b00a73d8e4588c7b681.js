e419b988c8bac48b0d4db5cd7b10301436179dc9
Siyu Long <i@longern.com>
2019-01-12 12:40:39 +0800

    Nuxt.js framework

---
+++ mini-ml/_nuxt/7b00a73d8e4588c7b681.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{333:function(e,n,t){"use strict";t.r(n);var i=t(23),s=Object(i.a)({},function(){var e=this.$createElement;return(this._self._c||e)("div",{attrs:{id:"app"}},[this._v("\n  Welcome\n")])},[],!1,null,null,null);s.options.__file="index.vue";n.default=s.exports}}]);
