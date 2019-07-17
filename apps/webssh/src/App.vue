2be0a5cbd6edc06c70d18af8f06d19c8e2e33a33
Siyu Long <i@longern.com>
2019-07-18 07:54:13 +0800

    Connect to server

--- apps/webssh/src/App.vue
+++ apps/webssh/src/App.vue
@@ -2,7 +2,8 @@
   <v-app>
     <v-content>
       <v-container fluid fill-height>
-        <v-layout align-center justify-center>
+        <terminal v-if="sshConnection"></terminal>
+        <v-layout v-else align-center justify-center>
           <login-card @input="handleLogin"></login-card>
         </v-layout>
       </v-container>
@@ -12,16 +13,29 @@
 
 <script>
 const LoginCard = httpVueLoader('src/components/LoginCard.vue')
+const Terminal = httpVueLoader('src/components/Terminal.vue')
+const ssh2 = window.require('ssh2')
 
 module.exports = {
+  data() {
+    return {
+      sshConnection: null
+    }
+  },
+
   methods: {
     handleLogin(config) {
-      console.log(config)
+      const conn = new ssh2.Client()
+      conn.on('ready', function() {
+        console.log('ready')
+      })
+      conn.connect(config)
     }
   },
 
   components: {
-    LoginCard
+    LoginCard,
+    Terminal
   }
 }
 </script>
