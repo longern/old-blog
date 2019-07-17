f6a37dc4712f717342b29b89b1d082dc18e23f63
Siyu Long <i@longern.com>
2019-07-18 00:04:23 +0800

    Add url and private key input

--- apps/webssh/src/components/LoginCard.vue
+++ apps/webssh/src/components/LoginCard.vue
@@ -5,13 +5,39 @@
     </v-toolbar>
     <v-card-text>
       <v-form>
-        <v-text-field name="login" label="Login" type="text"></v-text-field>
-        <v-text-field id="password" name="password" label="Password" type="password"></v-text-field>
+        <v-text-field v-model="url" label="URL" type="text"></v-text-field>
+        <v-text-field v-model="privateKey" label="Private Key" type="text"></v-text-field>
       </v-form>
     </v-card-text>
     <v-card-actions>
       <v-spacer></v-spacer>
-      <v-btn color="primary">Login</v-btn>
+      <v-btn color="primary" @click="handleLogin">Login</v-btn>
     </v-card-actions>
   </v-card>
 </template>
+
+<script>
+const fs = window.require('fs')
+const url = window.require('url')
+
+module.exports = {
+  data() {
+    return {
+      url: "ssh://",
+      privateKey: "",
+    }
+  },
+
+  methods: {
+    handleLogin() {
+      const sshUrl = url.parse(this.url)
+      this.$emit('input', {
+        host: sshUrl.host,
+        port: sshUrl.port || 22,
+        username: sshUrl.auth,
+        privateKey: privateKey
+      })
+    },
+  }
+}
+</script>
