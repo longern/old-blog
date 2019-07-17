3e6c409c35c0de683e030b1abfce9445d55f84b2
Siyu Long <i@longern.com>
2019-07-18 00:03:15 +0800

    Detect if config exists

--- apps/webssh/main.js
+++ apps/webssh/main.js
@@ -1,7 +1,17 @@
 (async function () {
 
+  const fs = require('fs')
+  const os = require('os')
+  const path = require('path')
+
   const h5native = require('h5native')
   const ssh2 = h5native.requireAsync('ssh2')
+  const sshConfig = h5native.requireAsync('ssh-config')
+
+  const sshConfigPath = path.join(os.homedir(), '.ssh/config')
+  if (fs.existsSync(sshConfigPath)) {
+    console.log(sshConfig.parse(fs.readFileSync(sshConfigPath)))
+  }
 
 })()
 
