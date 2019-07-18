617e68025bd97fe50f626c56a6c5d2d0eb287797
Longern <i@longern.com>
2019-07-18 09:04:31 +0800

    Fix missing await

--- apps/webssh/main.js
+++ apps/webssh/main.js
@@ -5,12 +5,12 @@
   const path = require('path')
 
   const h5native = require('h5native')
-  const ssh2 = h5native.requireAsync('ssh2')
-  const sshConfig = h5native.requireAsync('ssh-config')
+  const ssh2 = await h5native.requireAsync('ssh2')
+  const sshConfig = await h5native.requireAsync('ssh-config')
 
   const sshConfigPath = path.join(os.homedir(), '.ssh/config')
   if (fs.existsSync(sshConfigPath)) {
-    console.log(sshConfig.parse(fs.readFileSync(sshConfigPath)))
+    console.log(sshConfig.parse(fs.readFileSync(sshConfigPath, { encoding: 'utf-8' })))
   }
 
 })()
