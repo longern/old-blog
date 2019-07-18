95b779a42e8563c976c65c277f8120eb6c6dfd68
Siyu Long <i@longern.com>
2019-07-18 22:44:43 +0800

    Update terminal style

--- apps/webssh/src/components/Terminal.vue
+++ apps/webssh/src/components/Terminal.vue
@@ -1,6 +1,20 @@
 <template>
-  <div class="terminal"></div>
+  <div class="terminal" contenteditable></div>
 </template>
 
 <script>
 </script>
+
+<style>
+.terminal {
+  width: 100%;
+  height: 100%;
+
+  outline: none;
+  background: black;
+  color: whitesmoke;
+  caret-color: white;
+
+  font-family: Consolas, monospace;
+}
+</style>
